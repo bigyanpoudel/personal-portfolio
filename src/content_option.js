bc0c1fabaee6df5b5a402b7232efb2e9336a966a
bigyanpoudel <46457363+bigyanpoudel@users.noreply.github.com>
2023-01-10 15:12:52 +0545

    feat: personal website

---
+++ src/content_option.js
@@ -0,0 +1,235 @@
+const logotext = "BP";
+const meta = {
+  title: "Bigyan Poudel",
+  description: "I’m Bigyan Poudel Software devloper,currently working in Nepal",
+};
+
+const introdata = {
+  title: "I’m Bigyan Poudel",
+  animated: {
+    first: "I love coding and working in new technology",
+    second: "I code cool websites",
+    third: "I develop mobile apps",
+  },
+  description:
+    "I design and code beautifully simple things, and I love what I do.",
+  your_img_url: "./bigyan.jpg",
+};
+
+const dataabout = {
+  title: "About my self",
+
+  p: [
+    {
+      text: `
+      I am a FULL STACK DEVELOPER with more than 2 years of experience as a frontend engineer with React and Next.js, a backend developer with Node.js, and also have experience in building a mobile app using Flutter and React Native. "GOOD ARCHITECTURE AND CODE PRACTICES LEAD TOWARDS THE BETTER APPLICATION". That's what I am capable of doing. Developing the project from scratch, monitoring the development throughout the project, working the project with the latest technology and best practices, optimizing in terms of performance and SEO, and many more. I have a passion for creating responsive, user-friendly websites and am comfortable working with both front-end and back-end development. I also have a keen eye for pixel-perfect design to setting up the project architecture from the ground up, GitHub action, PWA app, Mobile app both ios and android, API integration and code performance optimization, SEO, deploying the mobile app, and many more.
+      `,
+    },
+    {
+      text: `I am well-versed in the latest web development trends and best practices and have a strong understanding of web design principles. I can turn a design into a fully-functional website, and I am able to write clean, maintainable, and efficient code.
+      `,
+    },
+    {
+      text: `I enjoy working in a collaborative environment and have experience working with cross-functional teams. I am able to communicate effectively with both technical and non-technical stakeholders.`,
+    },
+    {
+      text: `I am always seeking to improve my skills and stay current with the latest developments in web development. I have worked on many exciting projects and I would be happy to provide examples of my work upon request.
+      `,
+    },
+  ],
+};
+const worktimeline = [
+  {
+    jobtitle: "Full Stack Developer",
+    where: "Everestwalk Group",
+    date: "Oct 2020 - Oct 2021",
+  },
+  {
+    jobtitle: "React Developer",
+    where: "ServiceLab",
+    date: "Oct 2021 - April 2022",
+  },
+  {
+    jobtitle: "Full Stack Developer",
+    where: "WesionayTeam",
+    date: "April 2022 - current",
+  },
+];
+
+const skills = [
+  {
+    name: "React",
+    value: 90,
+  },
+  {
+    name: "NextJs",
+    value: 90,
+  },
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "Flutter",
+    value: 80,
+  },
+  {
+    name: "HTML",
+    value: 90,
+  },
+  {
+    name: "CSS",
+    value: 90,
+  },
+  {
+    name: "React Native",
+    value: 80,
+  },
+  {
+    name: "Nodejs",
+    value: 70,
+  },
+  {
+    name: "MongoDB",
+    value: 50,
+  },
+  {
+    name: "Express",
+    value: 70,
+  },
+  {
+    name: "Typescript",
+    value: 70,
+  },
+  {
+    name: "Go",
+    value: 25,
+  },
+];
+
+const services = [
+  {
+    title: "Web development",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Mobile Apps",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Wordpress Design",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "name@domain.com",
+  YOUR_FONE: "(555)123-4567",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/bigyanpoudel",
+  facebook: "https://www.facebook.com/profile.php?id=100004794825344",
+  linkedin: "https://www.linkedin.com/in/bigyanpoudel/",
+  twitter: "https://twitter.com/bigyan_poudel10",
+  medium: "https://medium.com/@bigyan.poudel",
+};
+
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
